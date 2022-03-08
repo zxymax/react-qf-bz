84f400e4f5685ccb2cc104a1dd08482dc808a3ef
zxymax <497084653@qq.com>
2022-03-08 18:38:24 +0800

    dangerouslySetInnerHtml的用法

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import RefComponent from './components/ref/RefComponent'
 import State from './components/state/State'
+import DangerouslySetInnerHtml from './components/dangerouslySetInnerHtml/DangerouslySetInnerHtml'
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, react</h1>
-       <RefComponent />
-       <State />
-  </div>
-  );
+      <RefComponent />
+      <State />
+      <DangerouslySetInnerHtml />
+    </div>
+  )
 }
 
-export default App;
+export default App

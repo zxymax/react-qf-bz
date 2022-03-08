803de6de7f3df2c41e7a3a52a1c75b7a71ebc861
zxymax <497084653@qq.com>
2022-03-08 18:01:10 +0800

    增加prettier配置

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+const options = {
+  arrowParens: 'avoid',
+  singleQuote: true,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  bracketSpacing: true,
+  semi: false,
+  trailingComma: 'none'
+}
+
+module.exports = options

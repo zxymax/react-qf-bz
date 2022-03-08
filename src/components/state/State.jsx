da490655b64987945ac823d550a2c85d4fce4acf
zxymax <497084653@qq.com>
2022-03-08 18:05:24 +0800

    增加state 添加删除功能

--- src/components/state/State.jsx
+++ src/components/state/State.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from 'react'
 
+function getR() {
+  return Math.random() * 100000
+}
+
 class State extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      val: '',
       name: '李秋水',
       list: [
-        { id: 0, name: '床前明月光'},
-        { id: 1, name: '疑是地上霜'},
-        { id: 2, name: '举头望明月'},
-        { id: 3, name: '低头思故乡'},
+        { id: getR(), name: '床前明月光' },
+        { id: getR(), name: '疑是地上霜' },
+        { id: getR(), name: '举头望明月' },
+        { id: getR(), name: '低头思故乡' }
       ]
     }
   }
@@ -27,17 +32,57 @@ class State extends Component {
     })
   }
 
+  addItem = () => {
+    this.setState({
+      list: [
+        ...this.state.list,
+        {
+          id: getR(),
+          name: this.state.val
+        }
+      ]
+    })
+  }
+
+  handleChange = e => {
+    this.setState({
+      val: e.target.value
+    })
+    console.log(e)
+  }
+
+  handleDelete = _id => {
+    this.setState(preState => {
+      const list = [...preState.list]
+      list.splice(_id, 1)
+      return { list }
+    })
+  }
+
   render() {
-    const { name, list } = this.state
-    const listData = list.length>0?list.map(item => <li key={item.id}>{item.name}</li>):(<p>no data</p>)
+    const { val, name, list } = this.state
+    const listData =
+      list.length > 0 ? (
+        list.map((item, index) => (
+          <li key={item.id}>
+            <strong>{item.id}</strong>
+            {item.name}{' '}
+            <button onClick={() => this.handleDelete(index)}>
+              delete item
+            </button>
+          </li>
+        ))
+      ) : (
+        <p>no data</p>
+      )
     return (
       <>
         <h1>作者：{name}</h1>
+        <input type="text" value={val} onChange={this.handleChange} />
         <button onClick={this.setAuthor}>改变新作者</button>&nbsp;
         <button onClick={this.removeSC}>删除诗词</button>
-        <ul>
-        {listData}
-      </ul>
+        <button onClick={this.addItem}>添加内容</button>
+        <ul>{listData}</ul>
       </>
     )
   }

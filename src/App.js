f90c8d0f26ed33467af8c5706def3cc0cde5f206
zxymax <497084653@qq.com>
2022-03-08 12:10:47 +0800

    ref的用法 最新用法createRef

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import RefComponent from './components/ref/RefComponent'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, react</h1>
+       <RefComponent />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/components/ref/RefComponent.jsx
@@ -0,0 +1,36 @@
+import React, { createRef, Component } from 'react'
+
+class RefComponent extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.inputRef = createRef()
+    this.firstWay = this.firstWay.bind(this)
+  }
+
+  firstWay() { // 需要绑定this 改变this指向 到组件 若不绑定this 值是undefined
+    console.log(this.inputRef.current.value)
+  }
+
+  secondWay = () => { // ES6语法 this指向 组件
+    console.log(this)
+    console.log(this.inputRef.current.value)
+
+    this.setState({
+      val: this.inputRef.current.value
+    })
+  }
+
+  render() {
+    return (
+      <>
+        <input type="text" ref={this.inputRef} />
+        <button onClick={this.firstWay}>First Way Get Input Value</button>
+        <button onClick={this.secondWay}>Second Way Get Input Value</button>
+      </>
+    )
+  }
+}
+
+export default RefComponent
